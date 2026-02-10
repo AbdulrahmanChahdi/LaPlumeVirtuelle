4efe00b6422e6fc85a3af4759cf82532b77240ae
Narvalo17 <102968094+Narvalo17@users.noreply.github.com>
2026-02-10 12:00:42 +0100

    fix(entity): remove unused imports from Abonnement and Categorie classes

--- Backend/src/main/java/com/example/laplumevirtuel/entities/Categorie.java
+++ Backend/src/main/java/com/example/laplumevirtuel/entities/Categorie.java
@@ -1,12 +1,10 @@
 package com.example.laplumevirtuel.entities;
 
-import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
